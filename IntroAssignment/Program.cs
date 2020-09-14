3877e324ae30e38aaa3c416553206fb66c547dbd
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-14 14:58:10 -0300

    Simple sum calculator

---
+++ IntroAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Programa para realizar a soma de dois números\n\nInforme o primeiro número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o segundo número: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"\nA soma de {n1} e {n2} é {n1 + n2}.\n");
+            if(Debugger.IsAttached)
+            {
+                Console.WriteLine("Pressione qualquer tecla para encerrar o programa...");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
