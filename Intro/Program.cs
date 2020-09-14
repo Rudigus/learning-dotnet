eb09a69036ba364cad2e652c956f0ce6486773ed
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-14 00:46:40 -0300

    First program of dell accessible learning tutorial

---
+++ Intro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha;
+            while ((linha = Console.ReadLine()) != null)
+            {
+                linha = "Linha digitada: " + linha;
+                Console.WriteLine(linha);
+            }
+        }
+    }
+}
