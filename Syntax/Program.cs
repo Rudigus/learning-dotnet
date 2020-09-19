5ab7340260ad2a7e4b94d62e58915bea9fd10a20
Rudigus <rodrigo.matos9@hotmail.com>
2020-09-19 20:21:58 -0300

    adding projects related to c# syntax

---
+++ Syntax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 10.5f;
+            float b = 15.5f;
+
+            int c = (int)(a + b);
+            int d = (int)a + (int)b;
+            Console.WriteLine($"a: {a}\nb: {b}\nc: {c}\nd: {d}");
+
+            string[] professors = {"1", "2"};
+            foreach (string professor in professors)
+            {
+                Console.WriteLine(professor);
+            }
+
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+    }
+}
---
+++ SyntaxAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular a média aritmética de uma determinada quantidade de números");
+            Console.Write("\nInforme a quantidade de números: ");
+            int quantNumeros = Convert.ToInt32(Console.ReadLine());
+            double media = 0;
+            Console.Write("\n");
+            for (int i = 0; i < quantNumeros; i++) 
+            {
+                Console.Write($"Informe o {i + 1}º número: ");
+                media += Convert.ToDouble(Console.ReadLine()) / quantNumeros;
+            }
+            Console.WriteLine($"\nA média aritmética dos números informados é, aproximada com duas casas decimais, igual a {media:0.00}");
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+    }
+}
