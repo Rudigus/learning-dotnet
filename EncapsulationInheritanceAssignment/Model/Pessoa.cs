f5e46842d089b7310d7e2e4946c3f12af36ffa4d
Rudigus <rodrigo.matos9@hotmail.com>
2020-10-19 16:04:02 -0300

    encapsulation and inheritance assignment done

---
+++ EncapsulationInheritanceAssignment/Model/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationInheritanceAssignment.Model
+{
+    class Pessoa
+    {
+        public string nome { get; set; }
+        public int telefone { get; set; }
+    }
+}
---
+++ EncapsulationInheritanceAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using EncapsulationInheritanceAssignment.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EncapsulationInheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PessoaJuridica jur = new PessoaJuridica();
+            jur.nome = "Pastelaria do Seu João";
+            Console.WriteLine(jur.nome);
+
+            PessoaFisica fis = new PessoaFisica();
+            fis.telefone = 987654321;
+            Console.WriteLine(fis.telefone);
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
